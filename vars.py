19374be261eb45108a607508fec1d129ba60baa6
German <salomeescalante9@gmail.com>
2022-11-19 13:04:37 -0500

    first commit

---
+++ vars.py
@@ -0,0 +1,34 @@
+# country_code = int('52')
+# print(country_code, type(country_code))
+
+# is_true = False
+# i_like_soda = False
+# print(is_true; i_like_soda)
+
+# var_1, var_2, var_3 = 0, 1, 3
+
+# print(var_2)
+# print(var_1)
+# print(var_3)
+
+# var_1, var_2, var_3 = 0
+
+# print(var_1)
+# print(var_2)
+# print(var_3)
+
+# print(None)
+
+# var_1 = var_2 = var_3 = 0
+
+# print(var_1)
+# print(var_2)
+# print(var_3)
+
+# print(None)
+
+def call():
+    global var
+    var = 1
+
+print(var)
